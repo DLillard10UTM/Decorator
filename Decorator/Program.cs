9fdf2eb9bbaea66f41aedcb8268d1d86b60a9e32
Daniel Lillard <danllill@ut.utm.edu>
2020-02-12 21:16:39 -0600

    Add project files.

---
+++ Decorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Widget wid;
+            BorderDecorator border;
+
+            wid = new TextField(4, 5);
+            border = new BorderDecorator(wid);
+
+            wid.draw();
+            border.draw();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Decorator/Widget.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Author: Danny Lillard
+ * Date: 2.12.2020
+ * Description: Practice with the decorator design pattern.
+ */
+namespace Decorator
+{
+    public interface Widget
+    {
+        public void draw();
+    }
+
+    public class TextField : Widget
+    {
+        int width;
+        int height;
+
+        public TextField(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public void draw()
+        {
+            Console.WriteLine("I am a text field! My values are: height: " + height + " width: " + width);
+        }
+    }
+
+    public abstract class Decorator : Widget
+    {
+        private Widget wid;
+
+        public Decorator(Widget w)
+        {
+            wid = w;
+        }
+        public void draw()
+        {
+            wid.draw();
+        }
+    }
+
+    public class BorderDecorator : Decorator
+    {
+        private Widget wid;
+        public BorderDecorator(Widget w) : base(w)
+        {
+            wid = w;
+        }
+
+        public void draw()
+        {
+            Console.WriteLine("Border Decorator! Containing: ");
+            base.draw();
+        }
+    }
+
+    public class ScrollDecorator : Decorator
+    {
+        public ScrollDecorator(Widget w) : base(w)
+        {
+
+        }
+
+        public new void draw()
+        {
+
+        }
+    }
+}
